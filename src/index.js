0234e14f7f09f73ebd54cef7d456a1d59aba4507
Thierry <thierry_192@outlook.com>
2020-11-03 22:23:07 -0300

    ajuste de identaçao

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const auth     = require('./controllers/index');
 const { isLoggedIn} = require('./middlewares/index');
 require('./auth/passport-setup');
 
-
 router.get('/', auth.home);
 router.get('/failed', auth.failed);
 router.get('/good', isLoggedIn, auth.good);
